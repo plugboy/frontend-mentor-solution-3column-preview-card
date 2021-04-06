3f1b342966cd49258bb31a82f617eaf6061be46f
Ho Chung Law (Center) <centerlaw@gmail.com>
2021-04-06 16:07:54 -0700

    refactored to iterate through a cars list

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,40 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
+
+  const cars = [
+    {
+      id: 1,
+      image: "/icon-sedans.svg",
+      title: "SEDANS",
+      description: "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
+    },
+    {
+      id: 2,
+      image: "/icon-suvs.svg",
+      title: "SUVS",
+      description: "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
+    },
+    {
+      id: 3,
+      image: "/icon-luxury.svg",
+      title: "LUXURY",
+      description: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
+    },
+  ];
+
   return (
     <div className={styles.container}>
-      <section className={styles.gridItem1}>
-        <div>
-          <img src={"/icon-sedans.svg"}/>
-          <h1>SEDANS</h1>
-          <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
-          <button>Learn More</button>
-        </div>
-      </section>
-      <section className={styles.gridItem2}>
-        <div>
-          <img src={"/icon-suvs.svg"}/>
-          <h1>SUVS</h1>
-          <p>Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</p>
-          <button>Learn More</button>
-        </div>
-      </section>
-      <section className={styles.gridItem3}>
-        <div>
-          <img src={"/icon-luxury.svg"}/>
-          <h1>LUXURY</h1>
-          <p>Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.</p>
-          <button>Learn More</button>
-        </div>
-      </section>
+      {cars.map(car => (
+        <section className={styles["gridItem" + car.id]}>
+          <div>
+            <img src={car.image}/>
+            <h1>{car.title}</h1>
+            <p>{car.description}</p>
+            <button>Learn More</button>
+          </div>
+        </section>
+      ))}
     </div>   
   )
 }
